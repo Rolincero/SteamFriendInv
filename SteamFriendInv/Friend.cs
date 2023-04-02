f49baec395fd0595f1961a9b2e0905fd947c6e64
Макс Сахапов <ms.mayron29@gmail.com>
2023-04-02 22:18:58 +1000

    Testing url response

---
+++ SteamFriendInv/Friend.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Net.Http;
+using System.Net;
+
+namespace SteamFriendInv
+{
+	internal class Friend
+	{
+		public void Get(string API, string steam_id)
+		{
+			string url = "https://api.steampowered.com/ISteamUser/GetFriendList/v1/?key=%7Bapi_key%7D&steamid=%7Bsteam_id%7D";
+
+			HttpClient client = new HttpClient();
+
+			client.GetAsync(url).Wait();
+            Console.WriteLine("Status: " + (int)client.GetAsync(url));
+        }
+	}
+}
