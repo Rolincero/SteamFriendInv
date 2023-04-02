d28baa6248ecb90e3c2bc892a7c35e83ba92eda5
Антон Давыдов <domeniqueflo@gmail.com>
2023-04-02 15:04:16 +0300

    Base Logger

---
+++ SteamFriendInv/services/Logger.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SteamFriendInv.services
+{
+    internal class Logger
+    {
+    }
+}
