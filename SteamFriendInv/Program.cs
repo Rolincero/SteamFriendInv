7e2767f830f1aa51cfa188a23dbd2cd65c159de0
Макс Сахапов <ms.mayron29@gmail.com>
2023-04-04 21:29:14 +1000

    Created tool for API cheking

--- SteamFriendInv/Program.cs
+++ SteamFriendInv/Program.cs
@@ -1,7 +1,9 @@
 ﻿using SteamFriendInv;
 
-await FriendDataProvider.Get
-	(
-	"2D436A4837330DAE1CBD96D1FC1C0683",
-	"76561198068488288"
-	);
+//await FriendDataProvider.Get
+//	(
+//	"2D436A4837330DAE1CBD96D1FC1C0683",
+//	"76561198068488288"
+//	);
+
+await APIFucker.LetsFuck();
---
+++ SteamFriendInv/Tools/APIFucker.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Diagnostics.Metrics;
+using System.IO.Pipes;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SteamFriendInv.Tools
+{
+    internal class APIFucker
+    {
+        private static string API = "2D436A4837330DAE1CBD96D1FC1C0683";
+        private static string ST_ID = "76561198068488288";
+        private static string url = $"http://steamcommunity.com/inventory/76561199026249930/570/2?l=english&count=5000";
+        private static HttpClient client = new HttpClient();
+        private static int Count = 0;
+        private static int BaseTime = 120000;
+        private static bool succes = false;
+        public static async Task LetsFuck()
+        {
+            while (true)
+            {
+                var response = await client.GetAsync(url);
+                try
+                {
+                    response.EnsureSuccessStatusCode();
+                }
+                catch (Exception Ex)
+                {
+                    Debug.WriteLine(Ex.Message);
+                }
+
+                var data = await response.Content.ReadAsStringAsync();
+                if (response.StatusCode is System.Net.HttpStatusCode.OK)
+                {
+                    Count++;
+                    Debug.WriteLine("Fucked");
+                    succes = true;
+                    Thread.Sleep(BaseTime);
+                    Logger();
+                }
+                else
+                {
+                    succes = false;
+                    //BaseTime += 5000;
+                    Thread.Sleep(BaseTime);
+                    Logger();
+                }
+            }
+        }
+
+        public static void Logger() => Debug.WriteLine($"Count: {Count} || BaseTime: {BaseTime} || Succes: {succes}");
+    }
+}
